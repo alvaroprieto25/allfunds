1e46f8f4844f63d1cf4e1f4de70eafa478854234
alvaroprieto25 <apm226@alu.ua.es>
2022-07-13 19:41:07 +0200

    Back style and upload front

---
+++ frontend/js/controllers/todoCtrl.js
@@ -0,0 +1,37 @@
+/*global angular */
+
+/**
+ * The main controller for the app. The controller:
+ * - retrieves and persists the model via the todoStorage service
+ * - exposes the model to the template and provides event handlers
+ */
+angular.module('mainApp')
+	.controller('TodoCtrl', function TodoCtrl($scope, $routeParams, $filter, store) {
+		'use strict';
+
+		var todos = $scope.todos = store.todos;
+
+		$scope.newTodo = '';
+		$scope.editedTodo = null;
+
+		$scope.$watch('todos', function () {
+			$scope.remainingCount = $filter('filter')(todos, { completed: false }).length;
+			$scope.completedCount = todos.length - $scope.remainingCount;
+			$scope.allChecked = !$scope.remainingCount;
+		}, true);
+
+		// Monitor the current route for changes and adjust the filter accordingly.
+		$scope.$on('$routeChangeSuccess', function () {
+			var status = $scope.status = $routeParams.status || '';
+			$scope.statusFilter = (status === 'active') ?
+				{ completed: false } : (status === 'completed') ?
+				{ completed: true } : {};
+		});
+
+		$scope.editTodo = function (todo) {
+			$scope.editedTodo = todo;
+			// Clone the original todo to restore it on demand.
+			$scope.originalTodo = angular.extend({}, todo);
+		};
+
+	});
